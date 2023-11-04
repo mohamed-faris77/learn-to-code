ec32cc85547f43201428f7a4a64d71f642268770
Mohamed Faris <134219037+mohamed-faris77@users.noreply.github.com>
2023-11-04 10:40:05 +0530

    simple-function.js

---
+++ simple-function.js
@@ -0,0 +1,12 @@
+var actor="Dhanush Raja";
+var player="ronaldo"
+var movie="oh my kadavule"
+
+function favorite() 
+{
+    console.log("favorite actor :"+actor)
+    console.log("favorite football player :"+player)
+    console.log("favorite movie :"+movie)
+}
+
+favorite()
