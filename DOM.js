6ab80d1e80793901a9b2787f3c958a84bf38e0c6
Mohamed Faris <134219037+mohamed-faris77@users.noreply.github.com>
2023-11-05 18:10:41 +0530

    DOM.js

---
+++ DOM.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>change value</title>
+</head>
+<body>
+    <h1 id="title">Hello world</h1>
+    <script>
+      var para = document.getElementById("title")
+      console.log(para.textContent)
+      para.textContent="Bye world"
+
+    </script>
+</body> 
+</html>
