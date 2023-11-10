a379c295e0a08425a445d9c3a0492b6ac55155dc
Mohamed Faris <134219037+mohamed-faris77@users.noreply.github.com>
2023-11-10 21:27:04 +0530

    Create addsub.js

---
+++ addsub.js
@@ -0,0 +1,21 @@
+
+function add(a, b) {
+  return a + b;
+}
+
+
+function subtract(a, b) {
+  return a - b;
+}
+
+
+var num1 = 10;
+var num2 = 5;
+
+
+var sum = add(num1, num2);
+console.log("Sum:", sum);
+
+
+var difference = subtract(num1, num2);
+console.log("Difference:", difference);
