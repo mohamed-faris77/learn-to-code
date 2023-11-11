de9780c0301d73d235540a2f58f68517a2b080a0
Mohamed Faris <134219037+mohamed-faris77@users.noreply.github.com>
2023-11-11 08:11:05 +0530

    Create simplearray.js

---
+++ simplearray.js
@@ -0,0 +1,10 @@
+
+var fruits = ['Apple', 'Banana', 'Orange', 'Mango'];
+
+
+console.log("First fruit:", fruits[0]);
+console.log("Second fruit:", fruits[1]); 
+
+
+fruits[2] = 'Grapes';
+console.log("Updated array:", fruits); 
