307e78ca47fb820c5775e716674d73ccf64ca672
Mohamed Faris <134219037+mohamed-faris77@users.noreply.github.com>
2023-12-10 17:33:47 +0530

    Array.js

---
+++ Array.js
@@ -0,0 +1,6 @@
+let numbers = [1, 2, 3, 4, 5];
+
+// Using Array.map() to double each number in the array
+let doubledNumbers = numbers.map(num => num * 2);
+
+console.log(doubledNumbers); // Output: [2, 4, 6, 8, 10]
